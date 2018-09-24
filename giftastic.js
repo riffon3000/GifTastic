728fb8c530ef09c52e0dceacd12ed99df99bcd04
riffon3000 <riffon@gmail.com>
2018-09-23 19:41:57 -0500

    update layout and javascript

--- giftastic.js
+++ giftastic.js
@@ -1,2 +1,66 @@
 // initial Array of search topics used to create buttons and search from GIPHY API
-var topics = ["dog", "cat", "batman", "omg", "what", "party"];
+var aTopics = ["dog", "cat", "batman", "omg", "what", "party"];
+
+// function call to display buttons of topics array when page loads
+createButtons();
+
+// function to display gifs after AJAX call to GIPHY API
+function displayGifs() {
+    $("#gifs").empty();
+    var name = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&limit=10&api_key=Wejq7Kof2nxeGqICbCSRGVOKcnTrjbo4";
+    // AJAX call for button being clicked 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response.data;
+            for (j = 0; j < results.length; j++) {
+                var topicsDiv = $('<a class="gifs">');
+                var p = $("<p>").text("Rating: " + results[j].rating);
+                var gif = $("<img>").attr("src", results[j].images.fixed_height_still.url);
+                gif.addClass("still");
+                topicsDiv.append(gif);  
+                topicsDiv.append(p);              
+                $("#gifs").append(topicsDiv);
+            }
+        });
+}
+//	Function to add user defined button from form
+$("#addTopic").click(function (event) {
+    event.preventDefault();
+    var topic = $("#topic-input").val().trim();
+    aTopics.push(topic);
+    $("#topic-input").val("");
+    createButtons();
+});
+
+//	Function to create buttons and display for items in topics array
+function createButtons() {
+    $("#topicButtons").empty();
+    for (var i = 0; i < aTopics.length; i++) {
+        var a = $("<button>");
+        a.addClass("topic-btn");
+        a.attr("data-name", aTopics[i]);
+        a.text(aTopics[i]);
+        a.attr("alt", aTopics[i]);
+        $("#topicButtons").append(a);
+    }
+}
+// click event to display gifs from topic buttons
+$(document).on("click", ".topic-btn", displayGifs);
+
+//	This function works to to start/stop gif on click as well (used original instead)
+$(document).on("click", "img", function () {
+    var src = $(this).attr("src");
+    if ($(this).hasClass('still')) {
+        //stop
+        $(this).attr('src', src.replace("_s.gif", ".gif"));
+        $(this).removeClass("still");
+    } else {
+        //play
+        $(this).attr('src', src.replace(".gif", "_s.gif"));
+        $(this).addClass("still");
+    }
+});
